0bb2885c7e2dc4ba6a6549fcec0396ff7bb54fae
Guannan Hu <108552122+Hu831@users.noreply.github.com>
2025-05-12 13:11:30 +0100

    Add files via upload

--- generate_ensemble_members.py
+++ generate_ensemble_members.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Generate ensemble of observation perturbations, ensemble of background error
-vector and ensemble of innovation vector.
-
-Author: Guannan Hu
-"""
-
 import numpy as np
 from H_operator_linear import H_operator_linear
 
-def generate_ensemble_members(n, nobs, N, xlon, xlat, ylon, ylat, Xb, R, d, errb, interpolator):
+def generate_ensemble_members(n, nobs, nens, xlon, xlat, ylon, ylat, Xb, R, d, errb, interpolator):
     """Generate ensemble of observation perturbations, ensemble of background error
     vector and ensemble of innovation vector.
     
@@ -20,7 +11,7 @@ def generate_ensemble_members(n, nobs, N, xlon, xlat, ylon, ylat, Xb, R, d, errb
         Number of model grid points.
     nobs : int
         Number of observations.
-    N : int
+    nens : int
         Ensemble size.
     xlon : n x 1 array
         Longitudes of model grid points.
@@ -53,22 +44,22 @@ def generate_ensemble_members(n, nobs, N, xlon, xlat, ylon, ylat, Xb, R, d, errb
     """
 
     # Generate observation perturbations for each ensemble member
-    obs_pert = np.random.multivariate_normal([0] * nobs, R, N).T
+    obs_pert = np.random.multivariate_normal([0] * nobs, R, nens).T
     sample_mean = np.mean(obs_pert, axis=1)
 
     # Remove bias due to sampling error
-    for k in range(N):
+    for k in range(nens):
         obs_pert[:, k] = obs_pert[:, k] - sample_mean  
         
     # Generate the innovation vector for each ensemble member
-    d_ens = np.zeros((nobs,N))
-    for k in range(N):
+    d_ens = np.zeros((nobs, nens))
+    for k in range(nens):
         interp = H_operator_linear(xlon, xlat, Xb[:,k], interpolator)
-        d_ens[:,k] = d + obs_pert[:,k] - interp(ylon,ylat)
+        d_ens[:,k] = d + obs_pert[:,k] - interp(ylon, ylat)
         
     # Generate the background error vector for each ensemble member
-    errb_ens = np.zeros((n,N))
-    for k in range(N):
+    errb_ens = np.zeros((n, nens))
+    for k in range(nens):
         errb_ens[:,k] = errb + Xb[:,k]
         
     return(obs_pert, d_ens, errb_ens)
